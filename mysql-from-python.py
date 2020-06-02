ec1b1130becc762e3700a9d37c9cfec34660c47d
ErikaN-ir <erika.nethaway@gmail.com>
2020-06-02 19:51:30 +0000

    Removed chinook

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -5,7 +5,7 @@ import pymysql
 username = os.getenv('C9_USER')
 
 # Connect to the database
-connection = pymysql.connect(host='localhost', user='username', password='', db='Chinook')
+connection = pymysql.connect(host='localhost', user=username, password='', db='Chinook')
 
 try:
     # Run a query
